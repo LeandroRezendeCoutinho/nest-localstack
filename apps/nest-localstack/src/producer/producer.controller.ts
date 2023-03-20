3582ed043346780545e3f143e8468bdc59aff677
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-03-20 09:06:04 -0300

    implemente producer send message

--- apps/nest-localstack/src/producer/producer.controller.ts
+++ apps/nest-localstack/src/producer/producer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Controller, Post, Body } from '@nestjs/common'
 import { ProducerService } from './producer.service'
 import { ProducerDto } from './dto/producer.dto'
 
--- apps/nest-localstack/src/producer/queue.service.ts
+++ apps/nest-localstack/src/producer/queue.service.ts
@@ -1,26 +1,39 @@
-import { CreateQueueCommand, SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
+import { CreateQueueCommand, ListQueuesCommand, SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
 import { Injectable } from "@nestjs/common"
-import { createQueueCommandInput, queueUrl, sqsClientConfig } from "../sqs-configs"
 
 @Injectable()
 export class QueueService {
     private sqsClient: SQSClient
     private command: CreateQueueCommand
+    private sqsConfig = {
+        region: 'us-east-1',
+        endpoint: 'http://localhost:4566/',
+    }
+
+    async listQueues() {
+        const client = new SQSClient(this.sqsConfig)
+        const command = new ListQueuesCommand({ QueueNamePrefix: 'test' })
+        return await client.send(command)
+    }
 
     async createQueue() {
-        this.sqsClient = new SQSClient(sqsClientConfig)
-        this.command = new CreateQueueCommand(createQueueCommandInput)
+        this.sqsClient = new SQSClient(this.sqsConfig)
+        this.command = new CreateQueueCommand({ QueueName: 'test-queue' })
         const response = await this.sqsClient.send(this.command)
         if (response.QueueUrl) {
             return response
         }
     }
 
-    async sendMessage(message: any) {
-        const sqsClient = new SQSClient(sqsClientConfig)
+    async sendMessage(body: any) {
+        const queueLlist = await this.listQueues()
+        if (queueLlist.QueueUrls?.length === 0)
+            await this.createQueue()
+
+        const sqsClient = new SQSClient(this.sqsConfig)
         const command = new SendMessageCommand({
-            QueueUrl: queueUrl,
-            MessageBody: message
+            QueueUrl: 'http://localhost:4566/000000000000/test-queue',
+            MessageBody: body.message
         })
         const response = await sqsClient.send(command)
         return response
